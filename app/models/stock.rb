29df638730df19e8a7fd1972b6c3ba781f4ddc97
Rajesh Patodia <rajesh.patodia@anchanto.com>
2021-02-18 21:21:53 +0530

    Add search stock and use ajax for form submission

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,6 +4,11 @@ class Stock < ApplicationRecord
     client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_token],
                                   secret_token: Rails.application.credentials.iex_client[:secret_token],
                                   endpoint: 'https://sandbox.iexapis.com/v1')
-    return client.price(ticker)
+    begin
+      new(ticker: ticker,name: client.company(ticker).company_name,last_price: client.price(ticker))
+    rescue=>exception
+      return nil
+    end
+    # return client.price(ticker)
   end
 end
